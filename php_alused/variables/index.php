cd59cd79c5ab6a7de90f4bd53e04c7be8f9be6dd
kert.metsaots <kert.metsaots@khk.ee>
2019-02-13 14:21:52 +0200

    muutuja test

---
+++ php_alused/variables/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kert.metsaots
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesnimi = 'Kert';
+$perenimi = 'Metsaots';
+$vanus = 22;
+$kaal = 80;
+
+//var_dump($eesnimi); muutuja sisu test
+
