c1c8a3cd5385b647995854ba23a48652167214f9
kert.metsaots <kert.metsaots@khk.ee>
2019-02-13 14:40:42 +0200

    sugu järgi värv

---
+++ php_alused/condition/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kert.metsaots
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesnimi = 'Kert';
+$perenimi = 'Metsaots';
+$vanus = 22;
+$kaal = 80;
+$sugu = 'mees';
+
+if($sugu == 'mees'){
+    echo '<p style="color:blue;">';
+} else {
+   echo '<p style="color:red;">';
+}
+
+//var_dump($eesnimi); muutuja sisu test
+
+echo 'minu eesnimi on '.$eesnimi.'<br>';
+echo 'minu perenimi on '.$perenimi.'<br>';
+echo 'minu vanus on '.$vanus.'<br>';
+echo 'minu kaal on '.$kaal.'<br>';
+
