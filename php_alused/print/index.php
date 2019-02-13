ed38aec04dd803939ed9ab1a6a53561221517745
kert.metsaots <kert.metsaots@khk.ee>
2019-02-13 13:11:30 +0200

    valgusfoori style

---
+++ php_alused/print/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="main">
+    <?php
+    echo '<div id="punane"></div>';
+    echo '<div id="kollane"></div>';
+    echo '<div id="roheline"></div>';
+    ?>
+</div>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: kert.metsaots
+ * Date: 13.02.2019
+ * Time: 13:08
+ */
