77168820030936aabca8fb1f4e9de25956d8aec5
kert.metsaots <kert.metsaots@khk.ee>
2019-02-20 14:05:28 +0200

    function - loodud tabeli jaoks vajalikud funktsioonid

--- php_alused/fuctions/index.php
+++ php_alused/fuctions/index.php
@@ -59,7 +59,7 @@ function tabeliRida($arv){
     echo '</tr>';
 }
 function tabel($ridadeArv){
-    echo '<table>';
+    echo '<table class="table">';
     echo tabeliPais();
     echo '<tbody>';
     for($reanumber = 0; $reanumber <= $ridadeArv; $reanumber++){
@@ -68,3 +68,22 @@ function tabel($ridadeArv){
     echo '</tbody>';
     echo '</table>';
 }
+?>
+    <!DOCTYPE html>
+    <html lang="et">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Arvud</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+              crossorigin="anonymous">
+    </head>
+    <body>
+    <div class="container text-center">
+        <!-- tabel -->
+<?php
+tabel(15);
+?>
