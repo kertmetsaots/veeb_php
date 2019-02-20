a06f69efccc9f6724e20759ca7386976c0a641a8
kert.metsaots <kert.metsaots@khk.ee>
2019-02-20 12:25:32 +0200

    function - paarsuse kontroll koos väljastusega

--- php_alused/fuctions/index.php
+++ php_alused/fuctions/index.php
@@ -1,9 +1,17 @@
 <?php
+// defineerime funktsiooni nimega paarsuseKontroll
 
+/**
+ * @param $arv
+ */
 function paarsuseKontroll($arv){
     if ($arv % 2 == 0) {
         echo $arv.'on paaris<br>';
     } else {
         echo $arv.' on paaritu<br>';
+
     }
 }
+
+//kutsume  funktsioon tööle
+paarsuseKontroll(7);
