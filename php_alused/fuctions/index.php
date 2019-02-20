b41a4aa71ba4aa0ff8960c2eb55fab6c34b1a6ab
kert.metsaots <kert.metsaots@khk.ee>
2019-02-20 14:03:26 +0200

    function - loodud tabeli jaoks vajalikud funktsioonid

--- php_alused/fuctions/index.php
+++ php_alused/fuctions/index.php
@@ -1,31 +1,70 @@
 <?php
-// defineerime funktsiooni nimega paarsuseKontroll
-
 /**
+ * function paarsuseKontroll
  * @param $arv
+ * print is $arv even or add
  */
 function kasPaaris($arv){
-    if ($arv % 2 == 0) {
+    if($arv % 2 == 0){
         $kontroll = true;
     } else {
         $kontroll = false;
-
     }
     return $kontroll;
 }
-
-function kirjeldus($paaris) {
-    if ($paaris == true) {
-        echo ' on paaris';
+function kasAlgarv($arv){
+    $jagaja = 2;
+    if($arv == 0 or $arv == 1) {
+        $kontroll = true;
     } else {
-        echo ' on paaritu';
+        while ($arv % $jagaja != 0) $jagaja++;
+        if ($arv == $jagaja) {
+            $kontroll = true;
+        } else {
+            $kontroll = false;
+        }
     }
-    echo '<br>';
+    return $kontroll;
 }
-
-//kutsume  funktsioon tööle
-for($arv = 0; $arv <= 10; $arv++) {
-    echo $arv;
-    $kontroll = kasPaaris($arv);
-    kirjeldus($kontroll);
+function tabeliPais(){
+    return '
+        <thead>
+                <tr>
+                    <th>arv</th>
+                    <th>paaris</th>
+                    <th>paaritu</th>
+                    <th>algarv</th>
+                </tr>
+        </thead>
+    ';
+}
+function tabeliRida($arv){
+    echo '<tr>';
+    echo '<td>'.$arv.'</td>';
+    echo '<td class="paaris">';
+    if(kasPaaris($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '<td class="paaritu">';
+    if(!kasPaaris($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '<td class="algarv">';
+    if(kasAlgarv($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '</tr>';
+}
+function tabel($ridadeArv){
+    echo '<table>';
+    echo tabeliPais();
+    echo '<tbody>';
+    for($reanumber = 0; $reanumber <= $ridadeArv; $reanumber++){
+        tabeliRida($reanumber);
+    }
+    echo '</tbody>';
+    echo '</table>';
 }
