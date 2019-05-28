79a5cc5b768b46398cabdc575f4841e14548e8b8
kert.metsaots <kert.metsaots@khk.ee>
2019-05-28 16:30:52 +0300

    oop- lisatud kasutaja sisse/välja logimise süstee,

---
+++ php_alused/date/index.php
@@ -0,0 +1,22 @@
+<?php
+//kuude massiiv
+$eesti_kuud = array(
+    1=> 'jaanuar',
+    'veebruar',
+    'märts',
+    'aprill',
+    'mai',
+    'juuni',
+    'juuli',
+    'august',
+    'september',
+    'oktoober',
+    'november',
+    'detsember'
+);
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;
