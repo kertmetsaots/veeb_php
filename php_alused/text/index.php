457fbad442e4ede7bf36732d64de359d60a81084
kert.metsaots <kert.metsaots@khk.ee>
2019-05-28 16:38:44 +0300

    oop- lisatud kasutaja sisse/välja logimise süstee,

---
+++ php_alused/text/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$tekst = 'Proovin Midagi Kirjutada';
+echo strtolower($tekst);
+echo '<br>';
+echo strtoupper($tekst);
+echo  '<br>';
+echo ucfirst(strtolower($tekst));
+echo '<br>';
+echo ucwords(strtolower($tekst));
