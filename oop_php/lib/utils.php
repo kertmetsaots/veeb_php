1c31f654195162375110bf64964e97ec44b98a1f
kert.metsaots <kert.metsaots@khk.ee>
2019-05-28 16:03:11 +0300

    oop- lisatud kasutaja sisse/välja logimise süstee,

--- oop_php/lib/utils.php
+++ oop_php/lib/utils.php
@@ -1,3 +1,4 @@
+
 <?php
 // wrapper function
 // change unsafe symbols to save versions
@@ -6,17 +7,14 @@ function fixHtml($val)
 {
     return htmlentities($val);
 }
-
 function fixUrl($val)
 {
     return urlencode($val);
 }
-
 function fixValue(&$val, $key)
 {
     $val = stripSlashes($val);
 }
-
 // add quotas to SQL query
 function fixDb($val)
 {
@@ -25,5 +23,9 @@ function fixDb($val)
     return '"'.mysql_real_escape_string($val, $db->conn).'"';
     */
     return '"'.addSlashes($val).'"';
+}
+//discount to menu price
+function discount($price,$procent){
+    return round($price - ($price * $procent) / 100.0, 2);
 }
 ?>
